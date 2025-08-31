10e7d723e401774964f7c1497f3246636ce1c5ad
replica <60418809+junobonnie@users.noreply.github.com>
2025-08-31 17:56:21 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -7,9 +7,10 @@ class Mail:
     def __init__(self):
         self.set_mail_config()
         self.set_mail_list()
-        self.msg = MIMEMultipart()
+        self.msg = MIMEMultipart("alternative")
         self.msg['From'] = self.mail_id
         self.image_count = 0
+        self.html_body = ""
 
     def login(self):
         self.server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -31,19 +32,45 @@ class Mail:
          text_part = MIMEText(text, "plain")
          self.msg.attach(text_part)
 
+    def add_text_html(self, text):
+        """HTML 본문에 텍스트/링크 추가"""
+        # 링크 자동 감지: 단순히 http/https를 포함한 텍스트는 a 태그로 감싸기
+        import re
+        def replace_link(match):
+            url = match.group(0)
+            return f'<a href="{url}">{url}</a>'
+        
+        html_text = re.sub(r'https?://[^\s]+', replace_link, text)
+        self.html_body += f'<p>{html_text}</p>\n'
+
     def add_img(self, image):
          self.image_count += 1
          with open(image, 'rb') as file:
              img = MIMEImage(file.read(), name=image)
          img.add_header('Content-ID', '<image%d>'%(self.image_count))
          self.msg.attach(img)
+         
+    def add_img_url(self, url, scale=0.5):
+        """
+        외부 URL 이미지 본문 삽입
+        scale: 0~1 사이, 이미지 크기 비율
+        """
+        self.html_body += f'<p><img src="{url}" alt="image" style="width:{scale*100}%"></p>\n'
 
     def clear(self):
-        self.msg = MIMEMultipart()
+        self.msg = MIMEMultipart("alternative")  # HTML + plain text 가능
+        self.msg['From'] = self.mail_id
         self.image_count = 0
+        self.html_body = ""
 
     def send(self):
         self.login()
+    
+        # HTML 본문 생성
+        if self.html_body:
+            html_part = MIMEText(self.html_body, "html")
+            self.msg.attach(html_part)
+    
         for mail_ in self.mail_list:
             self.msg['To'] = mail_
             self.server.sendmail(self.mail_id, mail_, self.msg.as_string())
